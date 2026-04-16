af7b7e806df3f91b62eb86c611a9f967f23accd7
skipper-switch <oluwatosin.adegoroye.interswitchng.com>
2026-04-16 08:22:05 +0100

    feat: table addedd

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -11,3 +11,21 @@ function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
 }
 
 export { Skeleton }
+
+
+
+interface TableSkeletonProps {
+    rows?: number;
+}
+
+const TableSkeleton: React.FC<TableSkeletonProps> = ({ rows = 5 }) => {
+    return (
+        <div className='space-y-2'>
+            {[...Array(rows)].map((_, index) => (
+                <Skeleton key={index++} className='h-6 w-full rounded-md' />
+            ))}
+        </div>
+    );
+};
+
+export { Skeleton, TableSkeleton };
